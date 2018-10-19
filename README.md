1a180659b8815748ebf5617507b76aceb1b5d296
Jake Witcher <jjwitcher09@gmail.com>
2018-10-18 20:06:55 -0400

    Add README, improve Main.elm

---
+++ README.md
@@ -0,0 +1,11 @@
+--Elm 0.19 Boilerplate for Browser.document Applications--
+
+Interested in creating an application in Elm v0.19 with more than just the simple "Browser.sandbox" and "Browser.element" examples found in the official guide? This boilerplate will provide you with a solid starting point for getting an app up and running in the browser. By using this boilerplate, you will be able to create a single web page and style your application using regular css files linked to the html page.
+
+To get started, clone this repository to your own machine, and type "elm init" in the terminal. This will prompt you to approve the creation of an elm.json file. Once this is complete you can begin writing your Elm code in the "Main.elm" file. Once you are ready to run the program in your browser, run the command "elm make src/Main.elm --output=main.js" in the terminal from the directory where the boilerplate was saved. This will compile your elm code to a JavaScript file. Open the index.html file in a browser to see your new web page!
+
+For additonal information refer to the official guide and package documentation: 
+
+https://guide.elm-lang.org/interop/
+https://guide.elm-lang.org/webapps/
+https://package.elm-lang.org/
--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,10 @@
-module Main exposing (..)
+module Main exposing (Document, Model, Msg(..), init, main, subscriptions, update, view)
 
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+
 
 main =
     Browser.document
@@ -19,12 +20,12 @@ main =
 
 
 type alias Model =
-    {}
+    { greeting : String }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
-    ({}, Cmd.none)
+    ( { greeting = "Hello Goodbye" }, Cmd.none )
 
 
 
@@ -32,40 +33,69 @@ init _ =
 
 
 type Msg
-    = 
+    = Hello
+    | Goodbye
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-   case msg of 
-   
-   {--each variant should return a model and a command message. If you are not using any command messages,
-   then use "Cmd.none". 
+    case msg of
+        Hello ->
+            ( { model | greeting = "I say \"hello\"" }, Cmd.none )
+
+        Goodbye ->
+            ( { model | greeting = "You say \"goodbye\"" }, Cmd.none )
+
+
+
+{--each variant should return a model and a command message. If you are not using any command messages,
+   then use "Cmd.none". This is different from what you may have done in "Browser.sandbox" if you started
+   with the official guide's tutorials.
    
    For example: 
    
    Variant -> 
         ( { model | key = changeToValue }, Cmd.none ) --}
-
-
 -- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
 
 
 -- VIEW
 
 
 type alias Document msg =
-  { title : String
-  , body : List (Html msg)
-  }
+    { title : String
+    , body : List (Html msg)
+    }
+
 
 view : Model -> Document Msg
 view model =
-    { title = ""
-    , body = []
+    { title = "Hello Goodbye"
+    , body =
+        [ div []
+            [ h1 [] [ text model.greeting ]
+            , button
+                [ type_ "Button"
+                , onClick
+                    (if model.greeting == "You say \"goodbye\"" then
+                        Hello
+
+                     else
+                        Goodbye
+                    )
+                ]
+                [ text "Click me!" ]
+            ]
+        ]
     }
+
+
+
+{--The body above must be a list, but you can delete everything else inside the square brackets
+    and write your own view code! --}
